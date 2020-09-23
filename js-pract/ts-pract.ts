02477c0cb564775f5b6873af61ad3af01450d86b
Ivan <64264529+cseltol@users.noreply.github.com>
2020-09-23 20:18:21 +0700

    Add JavaScript Practice

---
+++ js-pract/ts-pract.ts
@@ -0,0 +1,37 @@
+enum Color {Red = 1, Green, Blue};
+  let colorName: string = Color[2];
+  
+  console.log(colorName);
+
+for (var i = 0; i < 10; i++) {
+      // capture the current state of 'i'
+      // by invoking a function with its current value
+      (function(i) {
+          setTimeout(function() { console.log(i); }, 100 * i);
+      })(i);
+} 
+
+function sumMatrix(matrix: number[][]) {
+    let sum = 0;
+    for (let i = 0; i < matrix.length; i++) {
+        var currentRow = matrix[i];
+        for (let i = 0; i < currentRow.length; i++) {
+            sum += currentRow[i];
+        }
+    }
+ 
+    return sum;
+}
+
+function theCityThatAlwaysSleeps() {
+    let getCity;
+ 
+    if (true) {
+        let city = "Seattle";
+        getCity = function() {
+            return city;
+        }
+    }
+ 
+    return getCity();
+}
